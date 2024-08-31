361a47932c96fda0c5fa62a56cd7dcff55b94488
Nikolaev Ivan <60277@mail.ru>
2024-08-31 18:41:16 +0500

    Add files_and_dirs tools.py file Add function generator_files_in_dir

---
+++ tools/files_and_dirs.py
@@ -0,0 +1,9 @@
+import os
+
+
+def generator_files_in_dir(top_directory, extension=''):
+    for root, dirs, files in os.walk(top_directory):
+        for filename in filter(lambda tmp: tmp.endswith(extension), files):
+            filename_full = os.path.join(root, filename)
+            if os.path.isfile(filename_full) is True:
+                yield filename_full
