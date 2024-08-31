54988a9c321abdf7dbe710d3880a174b881bb1dc
Nikolaev Ivan <60277@mail.ru>
2024-08-31 19:04:40 +0500

    Добавлена строка документации к функции generator_files_in_dir

--- tools/files_and_dirs.py
+++ tools/files_and_dirs.py
@@ -2,6 +2,10 @@ import os
 
 
 def generator_files_in_dir(top_directory, extension=''):
+    """
+    Генератор имен файлов в некоторой директории
+    """
+
     for root, dirs, files in os.walk(top_directory):
         for filename in filter(lambda tmp: tmp.endswith(extension), files):
             filename_full = os.path.join(root, filename)
